611a474ee4358d824f2e207a7974d1b74df70c41
ntouev <evntouros@gmail.com>
2020-01-15 22:22:10 +0200

    server.py refinement

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import subprocess
 class ThreadedServer(object):
     def __init__(self, host, port):
         self.host = host
-        self.port = port to start a **client** run
+        self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
